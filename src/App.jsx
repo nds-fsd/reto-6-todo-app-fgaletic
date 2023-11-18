3e2b18afaff01fdc2f10cf3988e4fddbcfbf0a0f
filip.galetic@gmail.com <filip.galetic@gmail.com>
2023-11-19 00:14:29 +0100

    initial form built

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
+import styles from './App.module.css';
 
-import './App.css'
 
 function App() {
-
   return (
-    <div>
-      YOUR APP HERE
+    <div className={styles.container}>
+      <h1>Todo App</h1>
+        <label for="text">What needs to be done?</label>
+        <input type="text" />
+        <label for="date">Choose the due date</label>
+        <input type="date"/>
+        <label for="category">Choose a category</label>
+        <select name="category" id="category">
+          <option value="personal">Personal</option>
+          <option value="work">Work</option>
+          <option value="shopping">shopping</option>
+          <option value="health">Health</option>
+          <option value="family">Family</option>
+        </select>
+        {/* <input type="checkbox" /> */}
+        <button>Add</button>
     </div>
   );
-}
+};
 
 export default App
